1653651b574121221ef856558f74fc6079f05352
Faizankhan99 <usmani.faizan98@gmail.com>
2022-11-08 14:50:22 +0530

    add

---
+++ netmed_backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('hello'))
+
+app.listen(8080, () => {
+    console.log('server started on port 8080')
+})
