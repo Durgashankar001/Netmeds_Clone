3532bae188bfc94af595783d430b3318f9d5f216
Faizankhan99 <usmani.faizan98@gmail.com>
2022-11-08 14:53:59 +0530

    ad

--- netmed_backend/src/index.js
+++ netmed_backend/src/index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send('hello'))
 
-app.listen(8080, () => {
+app.listen(8080, async() => {
+    await mongoose.connect("mongodb://127.0.0.1:27017/netmeds")
     console.log('server started on port 8080')
 })
