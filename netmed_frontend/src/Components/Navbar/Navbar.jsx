d53c3f9ecc087f4b17fb870515ff6608863cc2b7
Faizankhan99 <usmani.faizan98@gmail.com>
2022-11-09 19:18:32 +0530

    addDay-2

--- netmed_frontend/src/Components/Navbar/Navbar.jsx
+++ netmed_frontend/src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,57 @@
-import { Box, Input, Image, Select,Text, TagLeftIcon } from "@chakra-ui/react";
+import { Box, Input, Image, Select, Text, TagLeftIcon } from "@chakra-ui/react";
 import styles from "./navbar.module.css";
-import {AiOutlineShoppingCart} from "react-icons/ai"
-import {CgProfile} from "react-icons/cg"
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { Link, useNavigate } from "react-router-dom";
 export default function Navbar() {
-    return (<>
-        <Box className={styles.main}>
-            <Box className={styles.imgBox}>
-                <Image alt="logo" />
-            </Box>
-            <Box className={styles.inpGroup}>
-                <Box>
-                    <Box>Delivered to</Box>
-                    <Select className={styles.selectme} placeholder='Select option' variant={"unstyled"} size="xs" w="110px">
-                        <option defaultValue={"110002"}>110002</option>
-                        <option value='option2'>Option 2</option>
-                        <option value='option3'>Option 3</option>
-                    </Select>
-                    <Box>|</Box>
-                    <Input ml={2} variant={"unstyled"} placeholder="Search for medicine and wellness products.." size={"xs"}/>
-                </Box>
-            </Box>
-            <Box className={styles.cartBox}>
-                <Box>
-                    <TagLeftIcon w={6} h={6} as={AiOutlineShoppingCart}/>
-                    <span>Cart</span>
-                    <span className={styles.itemCount}>0</span>
-                </Box>
-                <Box>
-                    <TagLeftIcon w={6} h={6} as={CgProfile}/>
-                    Sign in/Sign
-                </Box>
-            </Box>
+  const nav = useNavigate();
+
+  return (
+    <>
+      <Box className={styles.main}>
+        <Box onClick={() => nav("/")} className={styles.imgBox}>
+          {/* <Link to="/"> */}
+          <Image alt="logo" />
+          {/* </Link> */}
         </Box>
-    </>)
+        <Box className={styles.inpGroup}>
+          <Box>
+            <Box>Delivered to</Box>
+            <Select
+              className={styles.selectme}
+              placeholder="Select option"
+              variant={"unstyled"}
+              size="xs"
+              w="110px"
+            >
+              <option defaultValue={"110002"}>110002</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+            <Box>|</Box>
+            <Input
+              ml={2}
+              variant={"unstyled"}
+              placeholder="Search for medicine and wellness products.."
+              size={"xs"}
+            />
+          </Box>
+        </Box>
+        <Box className={styles.cartBox}>
+          <Box onClick={() => nav("/cart")}>
+            <TagLeftIcon w={6} h={6} as={AiOutlineShoppingCart} />
+            <span>Cart</span>
+            <span className={styles.itemCount}>0</span>
+          </Box>
+
+          {/* <Link to="/signup"> */}
+          <Box onClick={() => nav("/signup")}>
+            <TagLeftIcon w={6} h={6} as={CgProfile} />
+            Sign in/Sign
+          </Box>
+          {/* </Link> */}
+        </Box>
+      </Box>
+    </>
+  );
 }
--- netmed_frontend/src/main.jsx
+++ netmed_frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ChakraProvider } from "@chakra-ui/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
-)
+);
