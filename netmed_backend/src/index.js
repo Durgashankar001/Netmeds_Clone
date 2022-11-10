16dba6698135048948fced845330c667ec036631
saurabh solanki <solankisourabh25@gmail.com>
2022-11-10 20:09:07 +0530

    mongocon

--- netmed_backend/src/index.js
+++ netmed_backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const AuthData = require("../Router/User")
+const ProductData=require("../Router/Product")
 const cors =require("cors")
 
 const app = express()
@@ -10,10 +11,12 @@ app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use("/user",AuthData)
+app.use("/products",ProductData)
 
 app.get('/', (req, res) => res.send('hello'))
 
+
 app.listen(8080, async() => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/netmeds")
+    await mongoose.connect("mongodb+srv://metned:metned@cluster0.a4hekjg.mongodb.net/metned")
     console.log('server started on port 8080')
 })
